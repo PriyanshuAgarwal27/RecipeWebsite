15d5e9f1bfa7c3c143aeb329c77a9eae42f03e8f
Priyanshu <priyanshuagarwal610@gmail.com>
2024-03-25 00:57:34 +0530

    fixed formdata css

--- src/Components/SpecificRecipe.jsx
+++ src/Components/SpecificRecipe.jsx
@@ -19,33 +19,51 @@ const SpecificRecipe = () => {
 
   return (
     data && (
-      <center>
-        <h1 className="recipe-name"> {data.recipeName}</h1>
-
-        <div className="recipe-image">
-          <img alt="image" src={data.imageUrl}></img>
+      <div className="specific-recipe-container">
+        <div className="top-part">
+          <div className="left-top-part">
+            <h1 className="recipe-name"> {data.recipeName}</h1>
+            <Link
+              to={`/recipes?authorName=${data.authorName}`}
+              className="authorName"
+            >
+              {data.authorName}
+            </Link>
+          </div>
+          <div className="recipe-image">
+            <img alt="image" src={data.imageUrl}></img>
+          </div>
         </div>
-        <h2 className="description">description = {data.description}</h2>
-        <h2 className="tags">tags = {data.tags}</h2>
-        <h2 className="ingredients">
-          Ingredients
-          {Object.keys(data.ingredients).map((d) => {
-            return (
-              <div key={d}>
-                {d}: {data.ingredients[d]}
-              </div>
-            );
-          })}
-        </h2>
-        <h2 className="recipe-steps">recipe Steps = {data.recipeSteps}</h2>
-        <></>
-        <Link
-          to={`/recipes?authorName=${data.authorName}`}
-          className="authorName"
-        >
-          {data.authorName}
-        </Link>
-      </center>
+        <div className="description">
+          <h2 className="description-tag">Description </h2>
+          <span className="description-detail">{data.description}</span>
+        </div>
+        <div className="ingredients-class">
+          <h2 className="ingredients-tag">Ingredients </h2>
+          <div className="ingredients-desc">
+            {Object.keys(data.ingredients).map((d) => {
+              return (
+                <div key={d}>
+                  {d.toUpperCase()}: {data.ingredients[d]}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div>
+          <h2 className="recipe-steps-tag">Recipe Steps </h2>
+          <div> {data.recipeSteps}</div>
+        </div>
+        <div className="tag-container">
+          <h2 className="tags">Tags</h2>
+          <div>
+            {" "}
+            {data.tags.map((tag) => {
+              return <div className="single-tag">{tag}</div>;
+            })}
+          </div>
+        </div>
+      </div>
     )
   );
 };
--- src/Login/FormData.jsx
+++ src/Login/FormData.jsx
@@ -44,10 +44,16 @@ const FormData = (props) => {
     setTagValue(value);
   };
 
+  let tags;
   const onPressKeys = (e) => {
-    if (e.code === "Enter" || e.code === "Comma") {
-      setDisplayTag((prev) => [...prev, tagValue]);
-      setTagValue("");
+    if (e.code === "Comma" || e.code === "Enter") {
+      tags = tagValue.split(",");
+      if (e.code === "Enter") {
+        for (const eachTag in tags) {
+          setDisplayTag((prev) => [...prev, tags[eachTag]]);
+        }
+        setTagValue("");
+      }
     }
   };
   const onDeleteTag = (index) => {
@@ -97,24 +103,35 @@ const FormData = (props) => {
     };
     createRecipe(recipeData);
   };
-  const onTrueCheckbox = () => {
-    setIsChecked(false);
+  const onEnterIngredients = (e) => {
+    if (e.code === "Enter") {
+      handleAddIngredient();
+    }
   };
+
   return (
     <div className="form-container">
       <h2>Add a New Recipe</h2>
-      <label>
+      <div className="form-control">
+        <label htmlFor="recipeName" className="form-label">
+          Title
+        </label>
         <input
+          className="form-input"
           type="text"
           name="recipeName"
-          placeholder="Recipe Title"
+          placeholder="eg. Paneer Tikka Masala"
           value={inputValue.recipeName}
           id="recipeName"
           onChange={handleInputChange}
         />
-      </label>
-      <label>
+      </div>
+      <div className="form-control">
+        <label htmlFor="description" className="form-label">
+          Description
+        </label>
         <input
+          className="form-input"
           type="text"
           name="description"
           placeholder="Description"
@@ -122,10 +139,13 @@ const FormData = (props) => {
           id="description"
           onChange={handleInputChange}
         />
-      </label>
-      <label className="tag-label">
+      </div>
+      <div className="form-control">
+        <label className="form-label tag-label" htmlFor="tag">
+          Tag
+        </label>
         <input
-          className="input-tag"
+          className="form-input input-tag"
           type="text"
           name="tag"
           placeholder="Tag"
@@ -144,77 +164,89 @@ const FormData = (props) => {
             </div>
           );
         })}
-      </label>
-      <label>
+      </div>
+      <div className="form-control">
+        <label className="form-label" htmlFor="authorName">
+          {" "}
+          AuthorName
+        </label>
         <input
           type="text"
+          className="form-input"
           name="authorName"
           placeholder="Author"
           value={inputValue.authorName}
           id="authorName"
           onChange={handleInputChange}
         />
-      </label>
-      Ingredients:
-      {toShow &&
-        ingredientsValue.map((ingredient, index) => (
-          <label key={index} className="ingredients">
-            <input
-              className="ingredient-name"
-              type="text"
-              name="name"
-              placeholder="Name"
-              value={ingredient.name}
-              id="ingredientsKey"
-              onChange={(event) => handleIngredientsValue(event, index)}
-            />
-            <input
-              className="ingredient-quantity"
-              type="text"
-              name="quantity"
-              placeholder="Quantity"
-              value={ingredient.quantity}
-              id="ingredientsValue"
-              onChange={(event) => handleIngredientsValue(event, index)}
-            />
-            <button
-              className="onDeleteIngredients"
-              onClick={() => onDeleteIngredients(index)}
-            >
-              <img
-                width={"30px"}
-                height={"30px"}
-                alt="dustbinIcon"
-                src="https://thumb.ac-illust.com/3b/3b5b217ce258702c0956cbf3b146acba_t.jpeg"
-              ></img>
-            </button>{" "}
-          </label>
-        ))}
+      </div>
+      <div className="form-control">
+        <label className="form-label">Ingredients</label>
+        {toShow &&
+          ingredientsValue.map((ingredient, index) => (
+            <label key={index} className="ingredients">
+              <input
+                className="ingredient-name"
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={ingredient.name}
+                id="ingredientsKey"
+                onChange={(event) => handleIngredientsValue(event, index)}
+              />
+              <input
+                className="ingredient-quantity"
+                type="text"
+                name="quantity"
+                placeholder="Quantity"
+                value={ingredient.quantity}
+                id="ingredientsValue"
+                onKeyDown={onEnterIngredients}
+                onChange={(event) => handleIngredientsValue(event, index)}
+              />
+              <button
+                className="onDeleteIngredients"
+                onClick={() => onDeleteIngredients(index)}
+              >
+                <img
+                  width={"30px"}
+                  height={"30px"}
+                  alt="dustbinIcon"
+                  src="https://thumb.ac-illust.com/3b/3b5b217ce258702c0956cbf3b146acba_t.jpeg"
+                ></img>
+              </button>{" "}
+            </label>
+          ))}
+      </div>
       <button
         className="toAddIngredients"
         onClick={() => handleAddIngredient()}
       >
         + Add another ingredient
       </button>
-      <label>
-        <input
-          className="recipeSteps"
+      <div className="form-control">
+        <label className="form-label" htmlFor="recipeSteps">
+          Recipe Steps{" "}
+        </label>
+        <textarea
+          rows={5}
+          className="recipeSteps form-input"
           type="text"
           name="recipeSteps"
           placeholder="RecipeSteps"
           value={inputValue.recipeSteps}
           id="recipeSteps"
           onChange={handleInputChange}
-        />
-      </label>
-      <label className="checkbox-label">
-        <input
-          type="checkbox"
-          checked={isChecked}
-          onClick={() => setIsChecked(!isChecked)}
-        />{" "}
-        <span> {TERMS_AND_CONDITIONS} </span>
-      </label>
+        ></textarea>
+      </div>
+      <label className="checkbox-label form-label"></label>
+      <input
+        type="checkbox"
+        className="form-input"
+        checked={isChecked}
+        onChange={(e) => setIsChecked(e.target.checked)}
+      />{" "}
+      <span> {TERMS_AND_CONDITIONS} </span>
       <center>
         <button
           className="submit-button"
